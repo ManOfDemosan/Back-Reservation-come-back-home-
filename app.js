6ca1305e722ddb2456a0c7e9dc222d8996f96a78
ManOfDaemosan <hwanee0000@gmail.com>
2024-08-19 11:56:34 +0200

    [Feat] cors setting

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
+
+// CORS 설정
+app.use(
+    cors({
+        origin: 'https://comebackhomejaehwan.netlify.app', // 프론트엔드 도메인 허용
+        credentials: true, // 자격 증명을 포함한 요청을 허용하려면 설정
+    })
+);
+
+// JSON 파싱 미들웨어
+app.use(express.json());
 
 // 예약 라우트 파일 가져오기
 const reservationRoutes = require('./routes/reservations');
