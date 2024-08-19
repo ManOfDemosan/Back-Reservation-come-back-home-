68b2912769225189865f8cbc7218ffd3c530113f
ManOfDaemosan <hwanee0000@gmail.com>
2024-08-19 11:16:02 +0200

    change db

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('comebackhome', 'postgres', 'yourpassword', {
-    host: 'svc.sel4.cloudtype.app:30785',
+    host: 'svc.sel4.cloudtype.app', // 호스트는 포트 번호 없이 설정
     dialect: 'postgres',
-    port: 5432,
+    port: 30785, // 포트 번호는 따로 설정
 });
 
 sequelize
